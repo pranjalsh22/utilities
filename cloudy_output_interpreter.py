442086a58475d43ef2d07e7b171872cc1950dfb7
pranjalsh22 <physics0403@gmail.com>
2025-07-09 11:48:40 +0530

    Update cloudy_output_interpreter.py

--- cloudy_output_interpreter.py
+++ cloudy_output_interpreter.py
@@ -1,6 +1,6 @@
 import fluxtolum as flx
 import cloudy_o3line as o3
 
-purpose=st.selectbox(["process continuum ","find line strength"])
-if purpose=="process continuum ":
-    flx()
+#purpose=st.selectbox(["process continuum ","find line strength"])
+#if purpose=="process continuum ":
+flx()
