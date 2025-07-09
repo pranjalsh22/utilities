ae3b47afe0301b08eed5cbd382a54c91b52855fd
pranjalsh22 <physics0403@gmail.com>
2025-07-09 14:01:53 +0530

    Update cloudy_output_interpreter.py

--- cloudy_output_interpreter.py
+++ cloudy_output_interpreter.py
@@ -1,7 +1,3 @@
 import streamlit as st
 
-purpose=st.sidebar.multiselect("select",("process continuum ","find line strength"))
-if "process continuum " in purpose:
-  import fluxtolum 
-if purpose=="find line strength":
-  import cloudy_o3line
+"welcome" 
