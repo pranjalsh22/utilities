7e3fc1ac66e7dee99198cb007d31d80ca3f2a1c4
pranjalsh22 <physics0403@gmail.com>
2025-09-27 19:55:01 +0530

    Update Graphaway.py

--- Graphaway.py
+++ Graphaway.py
@@ -2,7 +2,8 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
-from scipy.integrate import simpson, trapezoid, interp1d
+from scipy.interpolate import interp1d
+from scipy.integrate import simpson, trapezoid
 
 
 # ------------------ Utilities ------------------
